1020a30669df70629f56040fef2a4b2c5ae17fb3
joao-vitor-vlr <61556746+joao-vitor-vlr@users.noreply.github.com>
2020-04-09 11:19:42 -0300

    Banco de dados

---
+++ banco de dados.sql
@@ -0,0 +1,26 @@
+create database projeto;
+use projeto;
+create table usuario(
+idUsuario int primary key auto_increment,
+login varchar(35),
+senha varchar(30),
+Nome varchar(35),
+tel_fixo int,    -- nao é obrigatorio
+tel_cel int,
+hospital int,
+foreign key (hospital) references Hospitais(idHospital),
+CNPJ int
+);
+create table Hospitais(
+idHospital int primary key auto_increment,
+nome_do_hospital varchar(30),
+endereço varchar(40),
+sensores int,
+foreign key (sensores) references Sensores(idSensor)
+);
+create table Sensores(
+idSensor int primary key,
+temperatura int,
+umidade int,
+lugar varchar(30)
+);
