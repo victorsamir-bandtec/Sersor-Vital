040998b171f822e757a827f565c85ea269e61421
Your Name <nata.silva@bandtec.com.br>
2020-06-10 16:54:16 -0300

    Colocado o node do yoshi para o nosso site, e pré ligamento com o banco de dados do Azure

---
+++ projeto-aquisicao-local/projeto-node-local/banco.js
@@ -0,0 +1,33 @@
+const configuracoes = {
+    banco: {
+        server: "serverhospital.database.windows.net",
+        user: "adminlocal",
+        password: "#Gfhosp1tal",
+        database: "BDHospital",
+        options: {
+            encrypt: true
+        },
+        pool: {
+            max: 4,
+            min: 1,
+            idleTimeoutMillis: 30000,
+            connectionTimeout: 5000
+        }
+    }
+}
+ 
+const sql = require('mssql');
+sql.on('error', err => {
+    console.error(`Erro de Conexão: ${err}`);
+});
+
+
+function conectar() {
+  sql.close();
+  return sql.connect(configuracoes.banco)
+} 
+
+module.exports = {
+    conectar: conectar,
+    sql: sql
+}
