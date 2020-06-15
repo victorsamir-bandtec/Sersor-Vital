3ff0d498e1001faa2ae36d0b7c5f4d268d7e7a1d
victorsamir-bandtec <victor.bsilva@bandtec.com.br>
2020-06-15 18:45:54 -0300

    tela suporte

--- HTML-02/app/database.js
+++ HTML-02/app/database.js
@@ -1,13 +1,13 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-    host     : 'localhost',
-    port     : '3306',
-    user     : 'teste',
-    password : 'marcelo12345',
-    database : 'svital'
+    host: 'localhost',
+    port: '3306',
+    user: 'root',
+    password: 'fond',
+    database: 'Svital'
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
     console.log('Conectado com sucesso!')
 });
